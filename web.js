a1fcda51254d518e575b142a983a3d0dac88a7a7
Manuel Comi <mcomi@hotmail.com>
2013-07-09 05:36:35 +0000

    Otra modificacion a Web.js porque no corria la aplicacion en heroku, al parecer no cerre la funcion app.get

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs'); 
 
 app.get('/', function(request, response) {
-  var myBuffer = fs.readFileSync('index.html','utf8');
-  var output = myBuffer.toString();
-  response.send(output);
+  var myBuffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
+  response.send(myBuffer.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
