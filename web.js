47cebe05b15a544963d05521a8b7854da3f16ac7
Manuel Comi <mcomi@hotmail.com>
2013-07-09 05:26:49 +0000

    Modifique web.js porque no corria la aplicacion en heroku

--- web.js
+++ web.js
@@ -5,14 +5,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs'); 
 
 app.get('/', function(request, response) {
-  var readIndex = fs.readFileSync('index.html', function(err,data){
-    if(err) throw err;
-    console.log(data);
-  });
-  buf = new Buffer(256);
-  console.log(buf.toString('utf8',0,readIndex);
-  response.send('Hello World 2!');
-});
+  var myBuffer = fs.readFileSync('index.html','utf8');
+  var output = myBuffer.toString();
+  response.send(output);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
