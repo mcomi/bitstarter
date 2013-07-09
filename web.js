c92aa5d09eeab4c63ac9b2c1654c569d385b0222
Manuel Comi <mcomi@hotmail.com>
2013-07-09 05:09:38 +0000

    Modify web.js to read index.html

--- web.js
+++ web.js
@@ -2,7 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs'); 
+
 app.get('/', function(request, response) {
+  var readIndex = fs.readFileSync('index.html', function(err,data){
+    if(err) throw err;
+    console.log(data);
+  });
+  buf = new Buffer(256);
+  console.log(buf.toString('utf8',0,readIndex);
   response.send('Hello World 2!');
 });
 
